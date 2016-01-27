e981ca0429c4bc939e081a6066eeaf2f7110cd1e
miujing <miujing@adsage.com>
2016-01-27 19:56:27 +0800

    update

--- src/main/scala/BinaryTrees.scala
+++ src/main/scala/BinaryTrees.scala
@@ -1,16 +1,35 @@
 package com.github.interaction.s99
 
 object binarytree{
-  sealed abstract class Tree[+T]
+  sealed abstract class Tree[+T]{
+    def isMirrorOf[V](tree: Tree[V]): Boolean
+    def isSymmetric: Boolean
+  }
   case class Node[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T] {
     override def toString = "T(" + value.toString + " " + left.toString + " " + right.toString + ")"
+
+    override def isMirrorOf[V](tree: Tree[V]): Boolean = tree match {
+      case t: Node[V] => left.isMirrorOf(t.right) && right.isMirrorOf(t.left)
+
+      case _ => false
+    }
+
+    override def isSymmetric: Boolean = left.isMirrorOf(right)
   }
+
   case object End extends Tree[Nothing] {
     override def toString = "."
+
+    override def isMirrorOf[V](tree: Tree[V]): Boolean = tree == End
+
+    override def isSymmetric: Boolean = true
   }
 
   object Node {
     def apply[T](value: T): Node[T] = Node(value, End, End)
+
+    def isSymmetric(node: Node): Boolean = false
+
   }
 
   object Tree {
